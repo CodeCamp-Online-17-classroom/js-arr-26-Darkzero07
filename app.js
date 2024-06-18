a01e3dd018c6cfd7c86d3056c231a9993d487a36
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-18 22:31:05 +0700

    js lab 112

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+
+const indexFine = tasks.findIndex((element) => {
+    return element.id === 2
+}) 
+
+console.log(indexFine)
